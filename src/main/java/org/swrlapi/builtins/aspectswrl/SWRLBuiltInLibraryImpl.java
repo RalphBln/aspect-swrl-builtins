3da5106c85f86f391996cc9aff27009229893184
Ralph Schaefermeier <ralph.schafermeier@gmail.com>
2022-03-21 16:27:52 +0100

    implement createOPA and some refactorings

--- src/main/java/org/swrlapi/builtins/aspectswrl/SWRLBuiltInLibraryImpl.java
+++ src/main/java/org/swrlapi/builtins/aspectswrl/SWRLBuiltInLibraryImpl.java
@@ -1,5 +1,7 @@
 package org.swrlapi.builtins.aspectswrl;
 
+import de.fuberlin.csw.aspectowl.owlapi.model.OWLAspectAssertionAxiom;
+import de.fuberlin.csw.aspectowl.owlapi.model.OWLJoinPointAxiomPointcut;
 import de.fuberlin.csw.aspectowl.owlapi.model.OWLOntologyAspectManager;
 import de.fuberlin.csw.aspectowl.owlapi.protege.AspectOWLEditorKitHook;
 import org.semanticweb.owlapi.model.*;
@@ -7,12 +9,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.swrlapi.builtins.AbstractSWRLBuiltInLibrary;
 import org.swrlapi.builtins.arguments.SWRLBuiltInArgument;
+import org.swrlapi.builtins.arguments.SWRLBuiltInArgumentType;
 import org.swrlapi.exceptions.SWRLBuiltInException;
 import org.swrlapi.exceptions.SWRLBuiltInLibraryException;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 import static org.swrlapi.builtins.arguments.SWRLBuiltInArgumentType.*;
 
@@ -59,77 +60,10 @@ public class SWRLBuiltInLibraryImpl extends AbstractSWRLBuiltInLibrary {
         // opArg can either be variable or concrete value
         // if variable, must be bound
         // either way, value must be an OWLObjectProperty
-        final OWLObjectProperty op;
-        if (opArg.isVariable()) {
-            var opVar = opArg.asVariable();
-            if (opVar.isUnbound()) {
-                throw new SWRLBuiltInException(String.format("Built-in %sopa in rule %s: Variable %s must be bound but is unbound.", NAMESPACE, getInvokingRuleName(), opVar.getVariableName()));
-            }
-            opArg = opVar.getBuiltInResult().get(); // we checked above that it's bound, so must have a value
-            if (opArg.getSWRLBuiltInArgumentType() != OBJECT_PROPERTY) {
-                throw new SWRLBuiltInException(String.format("Built-in %sopa in rule %s: Variable %s must be bound to a value of type %s but is %s.", NAMESPACE, getInvokingRuleName(), opVar.getVariableName(), OBJECT_PROPERTY, opArg.getSWRLBuiltInArgumentType()));
-            }
-            op = opArg.asSWRLObjectPropertyBuiltInArgument().getOWLObjectProperty();
-        } else {
-            if (opArg.getSWRLBuiltInArgumentType() != OBJECT_PROPERTY) {
-                throw new SWRLBuiltInException(String.format("Built-in %sopa in rule %s: First argument must be of type %s but is %s.", NAMESPACE, getInvokingRuleName(), OBJECT_PROPERTY, opArg.getSWRLBuiltInArgumentType()));
-            }
-            op = opArg.asSWRLObjectPropertyBuiltInArgument().getOWLObjectProperty();
-        }
-
-        final OWLNamedIndividual i1;
-        if (i1Arg.isVariable()) {
-            var i1Var = i1Arg.asVariable();
-            if (i1Var.isUnbound()) {
-                throw new SWRLBuiltInException(String.format("Built-in %sopa in rule %s: Variable %s must be bound but is unbound.", NAMESPACE, getInvokingRuleName(), i1Var.getVariableName()));
-            }
-            i1Arg = i1Var.getBuiltInResult().get(); // we checked above that it's bound, so must have a value
-            if (i1Arg.getSWRLBuiltInArgumentType() != NAMED_INDIVIDUAL) {
-                throw new SWRLBuiltInException(String.format("Built-in %sopa in rule %s: Variable %s must be bound to a value of type %s but is %s.", NAMESPACE, getInvokingRuleName(), i1Var.getVariableName(), NAMED_INDIVIDUAL, i1Arg.getSWRLBuiltInArgumentType()));
-            }
-            i1 = i1Arg.asSWRLNamedIndividualBuiltInArgument().getOWLNamedIndividual();
-        } else {
-            if (i1Arg.getSWRLBuiltInArgumentType() != NAMED_INDIVIDUAL) {
-                throw new SWRLBuiltInException(String.format("Built-in %sopa in rule %s: Second argument must be of type %s but is %s.", NAMESPACE, getInvokingRuleName(), NAMED_INDIVIDUAL, i1Arg.getSWRLBuiltInArgumentType()));
-            }
-            i1 = i1Arg.asSWRLNamedIndividualBuiltInArgument().getOWLNamedIndividual();
-        }
-
-        final OWLNamedIndividual i2;
-        if (i2Arg.isVariable()) {
-            var i2Var = i2Arg.asVariable();
-            if (i2Var.isUnbound()) {
-                throw new SWRLBuiltInException(String.format("Built-in %sopa in rule %s: Variable %s must be bound but is unbound.", NAMESPACE, getInvokingRuleName(), i2Var.getVariableName()));
-            }
-            i2Arg = i2Var.getBuiltInResult().get(); // we checked above that it's bound, so must have a value
-            if (i2Arg.getSWRLBuiltInArgumentType() != NAMED_INDIVIDUAL) {
-                throw new SWRLBuiltInException(String.format("Built-in %sopa in rule %s: Variable %s must be bound to a value of type %s but is %s.", NAMESPACE, getInvokingRuleName(), i2Var.getVariableName(), NAMED_INDIVIDUAL, i2Arg.getSWRLBuiltInArgumentType()));
-            }
-            i2 = i2Arg.asSWRLNamedIndividualBuiltInArgument().getOWLNamedIndividual();
-        } else {
-            if (i2Arg.getSWRLBuiltInArgumentType() != NAMED_INDIVIDUAL) {
-                throw new SWRLBuiltInException(String.format("Built-in %sopa in rule %s: Third argument must be of type %s but is %s.", NAMESPACE, getInvokingRuleName(), NAMED_INDIVIDUAL, i2Arg.getSWRLBuiltInArgumentType()));
-            }
-            i2 = i2Arg.asSWRLNamedIndividualBuiltInArgument().getOWLNamedIndividual();
-        }
-
-        final OWLClass aspect;
-        if (aspectArg.isVariable()) {
-            var aspectVar = aspectArg.asVariable();
-            if (aspectVar.isUnbound()) {
-                throw new SWRLBuiltInException(String.format("Built-in %sopa in rule %s: Variable %s must be bound but is unbound.", NAMESPACE, getInvokingRuleName(), aspectVar.getVariableName()));
-            }
-            aspectArg = aspectVar.getBuiltInResult().get(); // we checked above that it's bound, so must have a value
-            if (aspectArg.getSWRLBuiltInArgumentType() != CLASS) {
-                throw new SWRLBuiltInException(String.format("Built-in %sopa in rule %s: Variable %s must be bound to a value of type %s but is %s.", NAMESPACE, getInvokingRuleName(), aspectVar.getVariableName(), CLASS, aspectArg.getSWRLBuiltInArgumentType()));
-            }
-            aspect = aspectArg.asSWRLClassBuiltInArgument().getOWLClass();
-        } else {
-            if (aspectArg.getSWRLBuiltInArgumentType() != CLASS) {
-                throw new SWRLBuiltInException(String.format("Built-in %sopa in rule %s: Second argument must be of type %s but is %s.", NAMESPACE, getInvokingRuleName(), CLASS, aspectArg.getSWRLBuiltInArgumentType()));
-            }
-            aspect = aspectArg.asSWRLClassBuiltInArgument().getOWLClass();
-        }
+        final OWLObjectProperty op = getOWLObjectProperty(opArg, 1);
+        final OWLNamedIndividual i1 = getOWLNamedIndividual(i1Arg, 2);
+        final OWLNamedIndividual i2 = getOWLNamedIndividual(i2Arg, 3);
+        final OWLClass aspect = getOWLClass(aspectArg, 4);
 
         OWLOntology onto = getBuiltInBridge().getOWLOntology();
 
@@ -142,23 +76,115 @@ public class SWRLBuiltInLibraryImpl extends AbstractSWRLBuiltInLibrary {
         }
 
         return false;
-
     }
 
     /**
      * Satisfied iff the first argument is an OWL object property r, the second argument is an individual i1, the
-     * third argument is an individual i2 connected to i1 by r, and the fourth argument is an AspectOWL aspect name, in
-     * which the object property assertion r(i1, i2) holds.
+     * third argument is an individual i2 connected to i1 by r, and the fourth argument is an newly created AspectOWL
+     * aspect name, in which the object property assertion r(i1, i2) holds.
      *
      * @param arguments
      * @return
      * @throws SWRLBuiltInException
      */
     public boolean createOPA(List<SWRLBuiltInArgument> arguments) throws SWRLBuiltInException {
-        return false;
+        // argument 1: object property op
+        // argument 2: individual i1
+        // argument 3: individual i2
+        // argument 4: aspect a such that aspect(a, op(i1, i2))
+        checkNumberOfArgumentsEqualTo(4, arguments.size());
+        SWRLBuiltInArgument opArg = arguments.get(0);
+        SWRLBuiltInArgument i1Arg = arguments.get(1);
+        SWRLBuiltInArgument i2Arg = arguments.get(2);
+        SWRLBuiltInArgument aspectArg = arguments.get(3);
+
+        if (aspectArg.isVariable()) {
+            var aspectVar = aspectArg.asVariable();
+            if (aspectVar.isUnbound()) {
+                // opArg can either be variable or concrete value
+                // if variable, must be bound
+                // either way, value must be an OWLObjectProperty
+                final OWLObjectProperty op = getOWLObjectProperty(opArg, 1);
+                final OWLNamedIndividual i1 = getOWLNamedIndividual(i1Arg, 2);
+                final OWLNamedIndividual i2 = getOWLNamedIndividual(i2Arg, 3);
+
+                OWLOntology onto = getBuiltInBridge().getOWLOntology();
+
+                var joinPointAxiomOptional = onto.getObjectPropertyAssertionAxioms(i1).stream().filter(ax -> ax.getProperty().equals(op) && ax.getObject().equals(i2)).findFirst();
+
+                if (joinPointAxiomOptional.isPresent()) {
+                    // axiom exists, we cannot contextualize it
+                    return false;
+                }
+
+                OWLOntologyManager man = onto.getOWLOntologyManager();
+                OWLDataFactory df = man.getOWLDataFactory();
+
+                ArrayList<OWLAxiomChange> changes = new ArrayList<>();
+
+                OWLObjectPropertyAssertionAxiom joinPointAxiom = df.getOWLObjectPropertyAssertionAxiom(op, i1, i2);
+                changes.add(new AddAxiom(onto, joinPointAxiom));
+
+                OWLClass aspectClass = df.getOWLClass(IRI.create(onto.getOntologyID().getOntologyIRI().get().toString() + "#" + UUID.randomUUID().toString()));
+                OWLDeclarationAxiom aspectClassDeclarationAxiom = df.getOWLDeclarationAxiom(aspectClass);
+                changes.add(new AddAxiom(onto, aspectClassDeclarationAxiom));
+
+                man.applyChanges(changes);
+
+                OWLOntologyAspectManager aspectManager = AspectOWLEditorKitHook.getAspectManager(man);
+                OWLAspectAssertionAxiom aspectAssertionAxiom = aspectManager.getAspectAssertionAxiom(onto, new OWLJoinPointAxiomPointcut(joinPointAxiom), aspectManager.getAspect(aspectClass, Collections.EMPTY_SET, Collections.EMPTY_SET));
+                aspectManager.addAspect(onto, aspectAssertionAxiom);
+
+                aspectVar.setBuiltInResult(createClassBuiltInArgument(aspectClass));
+
+                return true;
+
+            } else {
+                // must be unbound
+                return false;
+            }
+        } else {
+            // must be variable
+            return false;
+        }
+
     }
 
-        @Override
+    private OWLNamedIndividual getOWLNamedIndividual(SWRLBuiltInArgument arg, int argPos) throws SWRLBuiltInException {
+        return checkAndGetActualArgument(arg, NAMED_INDIVIDUAL, argPos).asSWRLNamedIndividualBuiltInArgument().getOWLNamedIndividual();
+    }
+
+    private OWLClass getOWLClass(SWRLBuiltInArgument arg, int argPos) throws SWRLBuiltInException {
+        return checkAndGetActualArgument(arg, CLASS, argPos).asSWRLClassBuiltInArgument().getOWLClass();
+    }
+
+    private OWLObjectProperty getOWLObjectProperty(SWRLBuiltInArgument arg, int argPos) throws SWRLBuiltInException {
+        return checkAndGetActualArgument(arg, OBJECT_PROPERTY, argPos).asSWRLObjectPropertyBuiltInArgument().getOWLObjectProperty();
+    }
+
+    private SWRLBuiltInArgument checkAndGetActualArgument(SWRLBuiltInArgument arg, SWRLBuiltInArgumentType expectedType, int argPos) throws SWRLBuiltInException {
+        if (arg.isVariable()) {
+            var opVar = arg.asVariable();
+            if (opVar.isUnbound()) {
+                throw new SWRLBuiltInException(String.format("Built-in %sopa in rule %s: Variable %s must be bound but is unbound.", NAMESPACE, getInvokingRuleName(), opVar.getVariableName()));
+            }
+            SWRLBuiltInArgument actualArg = opVar.getBuiltInResult().get(); // we checked above that it's bound, so must have a value
+            if (actualArg.getSWRLBuiltInArgumentType() != expectedType) {
+                throw new SWRLBuiltInException(String.format("Built-in %sopa in rule %s: Variable %s must be bound to a value of type %s but is %s.", NAMESPACE, getInvokingRuleName(), opVar.getVariableName(), expectedType, actualArg.getSWRLBuiltInArgumentType()));
+            }
+            return actualArg;
+        } else {
+            if (arg.getSWRLBuiltInArgumentType() != expectedType) {
+                throw new SWRLBuiltInException(String.format("Built-in %sopa in rule %s: Argument at position %d must be of type %s but is %s.", NAMESPACE, getInvokingRuleName(), argPos, expectedType, arg.getSWRLBuiltInArgumentType()));
+            }
+            return arg;
+        }
+
+    }
+
+
+
+    @Override
     public void reset() throws SWRLBuiltInLibraryException {
 
     }
